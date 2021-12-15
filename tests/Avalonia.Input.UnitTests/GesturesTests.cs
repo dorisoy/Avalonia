a85572d1c3843f844c9483af68b799d3b754b2e0
Takoooooo <dan.pan2012@yandex.ru>
2021-12-15 17:13:47 +0200

    add test

--- tests/Avalonia.Input.UnitTests/GesturesTests.cs
+++ tests/Avalonia.Input.UnitTests/GesturesTests.cs
@@ -1,6 +1,11 @@
 using System.Collections.Generic;
 using Avalonia.Controls;
+using Avalonia.Input.Raw;
+using Avalonia.Layout;
+using Avalonia.Platform;
+using Avalonia.Rendering;
 using Avalonia.UnitTests;
+using Moq;
 using Xunit;
 
 namespace Avalonia.Input.UnitTests
@@ -8,7 +13,7 @@ namespace Avalonia.Input.UnitTests
     public class GesturesTests
     {
         private MouseTestHelper _mouse = new MouseTestHelper();
-        
+
         [Fact]
         public void Tapped_Should_Follow_Pointer_Pressed_Released()
         {
@@ -164,6 +169,45 @@ namespace Avalonia.Input.UnitTests
             _mouse.Down(border, MouseButton.Right, clickCount: 2);
 
             Assert.False(raised);
+        }
+        private class TestTopLevel : Window
+        {
+            private readonly ILayoutManager _layoutManager;
+            public bool IsClosed { get; private set; }
+
+            public TestTopLevel(IWindowImpl impl, ILayoutManager layoutManager = null)
+                : base(impl)
+            {
+                _layoutManager = layoutManager ?? new LayoutManager(this);
+            }
+
+            protected override ILayoutManager CreateLayoutManager() => _layoutManager;
+        }
+        [Fact]
+        public void PointerPressedEventArgs_ClickCount_Should_Work_With_Triple_Clicks()
+        {
+            var inputManagerMock = new Mock<InputManager>();
+            var services = TestServices.StyledWindow.With(inputManager: inputManagerMock.Object);
+
+            using (UnitTestApplication.Start(services))
+            {
+              
+                var impl = new Mock<IWindowImpl>(); 
+                impl.SetupAllProperties();
+
+                var target = new TestTopLevel(impl.Object);
+                var tb= new TextBox() { Width = 50, Height = 50 };
+                target.Content = tb;
+
+                var input = new RawPointerEventArgs(
+                    new Mock<MouseDevice>(null).Object,
+                    0,
+                    target,
+                   RawPointerEventType.XButton2Down,
+                   new Point(tb.Width / 2, tb.Height / 2), RawInputModifiers.None);
+                impl.Object.Input(input);
+
+            }
         }
 
         private void AddHandlers(
